d8bcf1796e24e0ba55cd006f86c50c6e28e9002c
Matt Player <mattplayer@mac.com>
2020-10-29 15:38:36 +0000

    add form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -9,20 +9,6 @@ import { Document } from "@contentful/rich-text-types"
 
 export const contactsQuery = graphql`
   query {
-    member : allContentfulCommitteeMember {
-      nodes {
-        name
-        position
-        image {
-          fluid {
-            src
-          }
-        }
-        description {
-          json
-        }
-      }
-    }
     images : allContentfulSiteImage(filter: {tags: {elemMatch: {tagName: {eq: "contacts"}}}}) {
       nodes {
         id
@@ -37,46 +23,30 @@ export const contactsQuery = graphql`
   }
 `
 
-interface MemberInterface {
-  image: { 
-    fluid: { 
-      src: string | undefined 
-    }
-  }
-  name: string; 
-  position: string; 
-  description: { 
-    json: Document 
-  }
-}
-
 const Contacts = () => {
-  const {member : {nodes : member}, images : {nodes : images}} = useStaticQuery(contactsQuery)
+  const {images : {nodes : images}} = useStaticQuery(contactsQuery)
   return (
     <Layout>
       <SEO title="Contact Us" />
       <HeroBanner image={images[0].imageSource.fluid.src} label="Contact Us"/>
-      <div className="commitee-members">
-        {member.map((member: MemberInterface) => (
-          <MemberDisplay {...member}/>
-        ))}
-      </div>
+      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label>Your Name: <input type="text" name="name"/></label>
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email"/></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </Layout>
   )
 }
 
 export default Contacts
-
-function MemberDisplay(member: MemberInterface) {
-  return <div className="member-display">
-    <div className="member-display_img-container">
-      <img src={member.image.fluid.src} alt={member?.name ?? undefined} />
-    </div>
-    <div className="member-display_info-container">
-      <h3>{member.name}</h3>
-      <h4>{member.position}</h4>
-      {documentToReactComponents(member.description.json)}
-    </div>
-  </div>
-}
 
